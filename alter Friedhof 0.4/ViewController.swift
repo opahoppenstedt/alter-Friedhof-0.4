9b6ae622faa3958484b96f7690728f2607bf6018
HiWi <hiwi@dot1x-242-012.wlan.uni-giessen.de>
2016-12-15 15:13:01 +0100

    Initial Commit

---
+++ alter Friedhof 0.4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  alter Friedhof 0.4
+//
+//  Created by HiWi on 15.12.16.
+//  Copyright © 2016 HiWi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
