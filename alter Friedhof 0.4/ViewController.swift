72a70e2343789291c5f7f746ed2cd5e7616535d7
opahoppenstedt <jonas.k.os@outlook.de>
2017-01-10 11:27:47 +0100

    Update ViewController.swift

--- alter Friedhof 0.4/ViewController.swift
+++ alter Friedhof 0.4/ViewController.swift
@@ -7,12 +7,49 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var map: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
+        
+        // mixedreality creating tiled overlays tutorial ##########
+
+        
+        //let midCoordinate:CLLocationCoordinate2D = CLLocationCoordinate2DMake(50.582345, 8.687494)
+        
+        
+        //working
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(50.582344, 8.687493)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+       
+        
+        map.setRegion(region, animated: true)
+        
+        let pin = PinAnnotation(title: "Alter Friedhof", subtitle: "Audiowalk", coordinate: location)
+        
+        map.addAnnotation(pin)
+        
+        //next Pin (Roentgen) ###########################
+        
+        let locationRoentgen:CLLocationCoordinate2D = CLLocationCoordinate2DMake(50.582444, 8.687555)
+        let pin2 = PinAnnotation(title: "Konrad Röntgen", subtitle: "der mit den Strahlen", coordinate: locationRoentgen)
+        map.addAnnotation(pin2)
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +59,3 @@ class ViewController: UIViewController {
 
 
 }
-
